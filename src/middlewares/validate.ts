38fedef876bce341dd5a05d5864ba4f4a0f29a6b
vtorvieira <vieira.vitor42@gmail.com>
2022-09-14 11:31:49 -0300

    feat(users): create validate users

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -41,6 +41,62 @@ const validate = {
     }
     next();
   },
+  validateUsernameUsers: (req: Request, res: Response, next: NextFunction) => {
+    const { username } = req.body;
+    
+    if (!username) {
+      throw new HttpException(400, '"username" is required');
+    }
+    if (typeof username !== 'string') {
+      throw new HttpException(422, '"username" must be a string');
+    }
+    if (username.length < 3) {
+      throw new HttpException(422, '"username" length must be at least 3 characters long');
+    }
+    next();
+  },
+  validateClasseUsers: (req: Request, res: Response, next: NextFunction) => {
+    const { classe } = req.body;
+    
+    if (!classe) {
+      throw new HttpException(400, '"classe" is required');
+    }
+    if (typeof classe !== 'string') {
+      throw new HttpException(422, '"classe" must be a string');
+    }
+    if (classe.length < 3) {
+      throw new HttpException(422, '"classe" length must be at least 3 characters long');
+    }
+    next();
+  },
+  validateLevelUsers: (req: Request, res: Response, next: NextFunction) => {
+    const { level } = req.body;
+    console.log(level);
+    if (!level && level !== 0) {
+      throw new HttpException(400, '"level" is required');
+    }
+    if (typeof level !== 'number') {
+      throw new HttpException(422, '"level" must be a number');
+    }
+    if (level < 1) {
+      throw new HttpException(422, '"level" must be greater than or equal to 1');
+    }
+    next();
+  },
+  validatePasswordUsers: (req: Request, res: Response, next: NextFunction) => {
+    const { password } = req.body;
+    
+    if (!password) {
+      throw new HttpException(400, '"password" is required');
+    }
+    if (typeof password !== 'string') {
+      throw new HttpException(422, '"password" must be a string');
+    }
+    if (password.length < 8) {
+      throw new HttpException(422, '"password" length must be at least 8 characters long');
+    }
+    next();
+  },
 };
 
 export default validate;
--- src/routes/usersRouters.ts
+++ src/routes/usersRouters.ts
@@ -2,10 +2,19 @@ import { Router } from 'express';
 
 import UsersController from '../controllers/usersController';
 
+import validate from '../middlewares/validate';
+
 const usersRouter = Router();
 
 const user = new UsersController();
 
-usersRouter.post('/', user.create);
+usersRouter.post(
+  '/', 
+  validate.validateUsernameUsers,
+  validate.validateClasseUsers,
+  validate.validateLevelUsers,
+  validate.validatePasswordUsers,
+  user.create,
+);
 
 export default usersRouter;
